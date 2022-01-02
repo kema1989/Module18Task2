9a8f578b71851ec403b180bc25e7e92a42f2b901
kema1989 <86516029+kema1989@users.noreply.github.com>
2022-01-02 12:16:51 +0600

    екрвкет

---
+++ Module18Task2/Command.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module18Task2
+{
+    abstract class Command
+    {
+        public abstract void Run();
+        public abstract void Cancel();
+    }
+}
---
+++ Module18Task2/DownloadCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module18Task2
+{
+    class DownloadCommand : Command
+    {
+        public override void Cancel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
